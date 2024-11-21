70bd20cfaeeccda4f003c68c964efcd08dcc34d7
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-11-21 11:15:41 +0100

    display crew in iss

--- iss_tracker.py
+++ iss_tracker.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 from datetime import datetime, timezone
+import requests
 
 req = urllib.request.Request("http://api.open-notify.org/iss-now.json")
 response = urllib.request.urlopen(req)
@@ -15,3 +16,19 @@ readable_time = datetime.fromtimestamp(timestamp, tz=timezone.utc).strftime('%Y-
 print(f"L'heure actuelle est : {readable_time}")
 print(f"La latitude de l'ISS est : {obj['iss_position']['latitude']}")
 print(f"La longitude de l'ISS est : {obj['iss_position']['longitude']}")
+
+# Récupérer les données des occupants de la station
+response = requests.get("http://api.open-notify.org/astros.json")
+if response.status_code == 200:
+    data = response.json()
+    num_people = data["number"]
+    people = data["people"]
+
+    # Afficher le nombre d'occupants
+    print(f"Il y a actuellement {num_people} personnes à bord de la Station Spatiale Internationale :")
+
+    # Afficher les noms des occupants
+    for person in people:
+        print(f"- {person['name']} ({person['craft']})")
+else:
+    print("Échec de la récupération des données.")
